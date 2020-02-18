61a2f12b1168115f806702215ae30d951ee6868d
dagpan <panagiotisntagkinis@gmail.com>
2020-02-18 13:38:18 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    if get('/name') { "My name is \_\_" }
+    end
+    if get('/hometown') { "My hometown is \_\_" }
+    end
+    if get('/favorite-song') { "My favorite song is \_\_" }
+    end
+
+
+
+
+
+
 end
